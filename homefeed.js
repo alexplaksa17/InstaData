e3eb0a3719dd173b75d9ba718748b71eaeba294a
alexplaksa17 <alexplaksa17@gmail.com>
2018-01-12 22:59:45 +0200

    add feed data

---
+++ homefeed.js
@@ -0,0 +1,9 @@
+[
+    {id:1, user:'best_user', photoUrl:'https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2017 || Podcast # 002 Posted 4 months ago'},
+    {id:2, user:'tratata', photoUrl:'https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg', text: 'Pavel Plastikk – Rebolledo Warm-up @ Closer Kyiv Reposted 6 months ago'},
+    {id:3, user:'reactRedux', photoUrl:'https://i1.sndcdn.com/artworks-000271428332-69hlk7-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2'},
+    {id:4, user:'fluxGeneration', photoUrl:'https://i1.sndcdn.com/artworks-000266246426-hkryuf-t500x500.jpg', text: 'stikk – Rebolledo'},
+    {id:5, user:'blackCity', photoUrl:'https://i1.sndcdn.com/artworks-000262731806-v89zol-t500x500.jpg', text: 'up @ Closer Kyiv Reposted 6 '},
+    {id:6, user:'Closer', photoUrl:'https://i1.sndcdn.com/artworks-000239218956-yddow9-t500x500.jpg', text: 'and 002 Posted 4 '},
+    {id:7, user:'rhythmBuro', photoUrl:'https://i1.sndcdn.com/artworks-000222854394-ytgyts-t500x500.jpg', text: 'or Glushko - Bra'},
+  ]
