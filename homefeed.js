a0a9781c4054d4d15da5af750b3057cfc1828289
Aleksandr Plaksa <alexplaksa17@gmail.com>
2018-01-13 21:08:28 +0200

    update data

--- homefeed.js
+++ homefeed.js
@@ -1,9 +1,9 @@
 [
-    {id:1, user:'best_user', photoUrl:'https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2017 || Podcast # 002 Posted 4 months ago'},
-    {id:2, user:'tratata', photoUrl:'https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg', text: 'Pavel Plastikk – Rebolledo Warm-up @ Closer Kyiv Reposted 6 months ago'},
-    {id:3, user:'reactRedux', photoUrl:'https://i1.sndcdn.com/artworks-000271428332-69hlk7-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2'},
-    {id:4, user:'fluxGeneration', photoUrl:'https://i1.sndcdn.com/artworks-000266246426-hkryuf-t500x500.jpg', text: 'stikk – Rebolledo'},
-    {id:5, user:'blackCity', photoUrl:'https://i1.sndcdn.com/artworks-000262731806-v89zol-t500x500.jpg', text: 'up @ Closer Kyiv Reposted 6 '},
-    {id:6, user:'Closer', photoUrl:'https://i1.sndcdn.com/artworks-000239218956-yddow9-t500x500.jpg', text: 'and 002 Posted 4 '},
-    {id:7, user:'rhythmBuro', photoUrl:'https://i1.sndcdn.com/artworks-000222854394-ytgyts-t500x500.jpg', text: 'or Glushko - Bra'},
-  ]
+  {id:1, user:'get', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2017 || Podcast # 002 Posted 4 months ago'},
+  {id:2, user:'post', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg', text: 'Pavel Plastikk â€“ Rebolledo Warm-up @ Closer Kyiv Reposted 6 months ago'},
+  {id:3, user:'create', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000127136412-qn03r5-t500x500.jpg', text: 'Igor Glushko - Brave! Factory Festival 2'},
+  {id:4, user:'read', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000096929679-u5d4f8-t500x500.jpg', text: 'stikk â€“ Rebolledo'},
+  {id:5, user:'update', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000243553452-86w0j9-t500x500.jpg', text: 'up @ Closer Kyiv Reposted 6 '},
+  {id:6, user:'delete', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000196091767-sldg1u-t500x500.jpg', text: 'and 002 Posted 4 '},
+  {id:7, user:'oneMoreUser', userIcon:'https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg', photoUrl:'https://i1.sndcdn.com/artworks-000246492137-jjaddr-t500x500.jpg', text: 'or Glushko - Bra'},
+]
