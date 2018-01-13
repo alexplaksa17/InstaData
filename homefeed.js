b1c784f292d754f3f81cb149e1331fda2109dacf
Aleksandr Plaksa <alexplaksa17@gmail.com>
2018-01-13 22:54:26 +0200

    Update homefeed.js

--- homefeed.js
+++ homefeed.js
@@ -5,14 +5,14 @@
      "user": "get",
      "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
      "photoUrl": "https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg",
-     "text": "Igor Glushko - Brave! Factory Festival 2017 || Podcast # 002 Posted 4 months ago"
+     "text": "text1"
     },
     {
       "id": 2,
       "user": "post",
       "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
       "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
-      "text": "Pavel Plastikk – Rebolledo Warm-up @ Closer Kyiv Reposted 6 months ago"
+      "text": "text2"
     }
   ]
 }
