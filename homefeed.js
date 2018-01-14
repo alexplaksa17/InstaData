7a84403f575aa62d3c44ab95612d1759ae222223
Aleksandr Plaksa <alexplaksa17@gmail.com>
2018-01-14 04:35:42 +0200

    Update homefeed.js

--- homefeed.js
+++ homefeed.js
@@ -5,7 +5,7 @@
      "user": "getA",
      "userIcon": "https://i1.sndcdn.com/artworks-000138719475-048wtj-t500x500.jpg",
      "photoUrl": "https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg",
-     "text": "text1"
+     "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
     {
       "id": 2,
