e921d34e972d0f8354e2fef04c37138306f8a15a
Aleksandr Plaksa <alexplaksa17@gmail.com>
2018-01-15 00:08:52 +0200

    Update homefeed.js

--- homefeed.js
+++ homefeed.js
@@ -1,14 +1,14 @@
 {
   "data" : [
     {
-     "id": 1,
+     "id": 5,
      "user": "firstUser",
      "userIcon": "https://i1.sndcdn.com/artworks-000138719475-048wtj-t500x500.jpg",
      "photoUrl": "https://i1.sndcdn.com/artworks-000209742602-bmyqmy-t500x500.jpg",
      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
     {
-      "id": 2,
+      "id": 4,
       "user": "secondUser",
       "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
       "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
@@ -22,14 +22,14 @@
       "text": "text3"
     },
     {
-      "id": 4,
+      "id": 2,
       "user": "thirdOne",
       "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
       "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
       "text": "text4"
     },
     {
-      "id": 5,
+      "id": 1,
       "user": "oneMoreUser",
       "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
       "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
