cccf7776a32f809d1a65358c0b26d1494d466b39
Aleksandr Plaksa <alexplaksa17@gmail.com>
2018-01-15 03:26:46 +0200

    Update homefeed.js

--- homefeed.js
+++ homefeed.js
@@ -10,30 +10,30 @@
     {
       "id": 4,
       "user": "secondUser",
-      "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
-      "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
-      "text": "text2"
+      "userIcon": "https://i1.sndcdn.com/artworks-000243553453-7dq6ff-t500x500.jpg",
+      "photoUrl": "https://i1.sndcdn.com/artworks-000203974558-82aeyp-t500x500.jpg",
+      "text": "Redux is a predictable state container for JavaScript apps."
     },
     {
       "id": 3,
       "user": "firstUser",
-      "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
-      "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
-      "text": "text3"
+      "userIcon": "https://i1.sndcdn.com/avatars-000312946554-pp3jnu-t120x120.jpg",
+      "photoUrl": "https://i1.sndcdn.com/artworks-000104728925-im0y74-t500x500.jpg",
+      "text": "You can use Redux together with React, or with any other view library."
     },
     {
       "id": 2,
       "user": "thirdOne",
-      "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
-      "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
-      "text": "text4"
+      "userIcon": "https://i1.sndcdn.com/avatars-000292118851-eadx2p-t120x120.jpg",
+      "photoUrl": "https://i1.sndcdn.com/artworks-000179849277-6cdu7z-t500x500.jpg",
+      "text": "Redux evolves the ideas of Flux, but avoids its complexity by taking cues from Elm. Whether you have used them or not, Redux only takes a few minutes to get started with."
     },
     {
       "id": 1,
       "user": "oneMoreUser",
-      "userIcon": "https://i1.sndcdn.com/artworks-000164359348-1noioy-t120x120.jpg",
-      "photoUrl": "https://i1.sndcdn.com/artworks-000147477275-my3dmh-t500x500.jpg",
-      "text": "text5"
+      "userIcon": "https://i1.sndcdn.com/artworks-000129315878-15vifz-t120x120.jpg",
+      "photoUrl": "https://i1.sndcdn.com/artworks-000136210589-y01xe2-t500x500.jpg",
+      "text": "React is a JavaScript library for building user interfaces."
     }    
   ]
 }
